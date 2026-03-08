d90c4355e44be9d87883e0b8976d242287bf65f1
Jacek Kurowski <jacek@4dl.pl>
2026-03-08 13:36:35 +0100

    [New][Jacek] Inwestycje zrealizowane

--- resources/views/front/developro/completed/index.blade.php
+++ resources/views/front/developro/completed/index.blade.php
@@ -7,75 +7,44 @@
 
 @section('content')
     <!-- MAIN SECTION -->
-    <main class="overflow-hidden">
-        <!-- BREADCRUMB -->
-        <div class="container breadcrumb-section">
+    <div id="page">
+        <div class="container">
             <div class="row">
-                <div class="col-12">
-                    <a href="/">Strona główna</a> / <a href="/o-nas/">O nas</a> / Inwestycje zrealizowane
-                </div>
-            </div>
-        </div>
-        <!-- PAGE ENTRY -->
-        <div class="container-fluid page-entry-center mt-5 position-relative">
-            <div class="container ">
-                <div class="row">
-                    <div class="col-12 col-lg-10 col-xl-8 col-xxl-6 offset-0 offset-lg-1 offset-xl-2 offset-xxl-3  d-flex flex-column align-items-center justify-content-center order-2 order-xl-1 scroll-anim-top">
-                        <h1 class="text-uppercase text-center">Inwestycje</br>zrealizowane</h1>
-                        <p class="mt-10 mb-0 text-center">Niektóre z naszych realizacji to kameralne osiedla, inne – nowoczesne budynki wielorodzinne, natomiast wszystkie łączy jedno: solidność wykonania i zadowolenie nabywców, którzy nazwali je swoim domem.</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <div class="container-fluid zrealizowane mt-5">
-            <div class="container">
-                <div class="row">
-                    @foreach($investments as $i)
-                        <div class="col-12 col-md-6 col-xl-4 mb-15 mb-sm-30 scroll-anim-bottom">
-                            <div class="zrealizowane__item position-relative">
-                                @if(1 == 2)
+                @foreach($investments as $i)
+                    <div class="col-12 col-md-6 col-xl-4 mb-15 mb-sm-30 scroll-anim-bottom">
+                        <div class="zrealizowane">
+                            @if(1 == 2)
                                 <a href="{{ route('front.developro.completed.show', $i->slug) }}" class="zrealizowane__streched-link z-2"></a>
-                                @endif
-                                <div class="d-flex position-relative">
-                                    <img class="zrealizowane__img w-100" src="{{ asset('/investment/thumbs/'. $i->file_thumb ) }}" width="300" height="250" alt="Wizualizacja inwestycji {{ $i->name }}">
-                                </div>
-                                <div class="d-flex flex-column align-items-start justify-content-start p-20">
-                                    <h3 class="text-uppercase mb-10">{{ $i->name }}</h3>
-                                    <span class="mb-15">{{ $i->inv_city }}, {{ $i->inv_street }}</span>
-                                    <span class="mb-15">Rok ukończenia: {{ $i->date_end }} r</span>
-                                    @if(1 == 2)
+                            @endif
+                            <div class="d-flex position-relative zrealizowane-img">
+                                <img src="{{ asset('/investment/thumbs/'. $i->file_thumb ) }}" width="456" height="380" alt="Wizualizacja inwestycji {{ $i->name }}">
+                            </div>
+                            <div class="d-flex flex-column align-items-start justify-content-start p-20">
+                                <h3 class="text-uppercase mb-10">{{ $i->name }}</h3>
+                                <ul class="mb-0 list-unstyled">
+                                    <li>
+                                        <svg width="19px" height="19px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M12 13C13.6569 13 15 11.6569 15 10C15 8.34315 13.6569 7 12 7C10.3431 7 9 8.34315 9 10C9 11.6569 10.3431 13 12 13Z" stroke="#939393" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                            <path d="M12 22C16 18 20 14.4183 20 10C20 5.58172 16.4183 2 12 2C7.58172 2 4 5.58172 4 10C4 14.4183 8 18 12 22Z" stroke="#939393" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                        </svg>
+                                        {{ $i->inv_city }}, {{ $i->inv_street }}
+                                    </li>
+                                    <li>
+                                        <svg width="19px" height="18px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M3 9H21M17 13.0014L7 13M10.3333 17.0005L7 17M7 3V5M17 3V5M6.2 21H17.8C18.9201 21 19.4802 21 19.908 20.782C20.2843 20.5903 20.5903 20.2843 20.782 19.908C21 19.4802 21 18.9201 21 17.8V8.2C21 7.07989 21 6.51984 20.782 6.09202C20.5903 5.71569 20.2843 5.40973 19.908 5.21799C19.4802 5 18.9201 5 17.8 5H6.2C5.0799 5 4.51984 5 4.09202 5.21799C3.71569 5.40973 3.40973 5.71569 3.21799 6.09202C3 6.51984 3 7.07989 3 8.2V17.8C3 18.9201 3 19.4802 3.21799 19.908C3.40973 20.2843 3.71569 20.5903 4.09202 20.782C4.51984 21 5.07989 21 6.2 21Z" stroke="#939393" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                        </svg>
+                                        Rok ukończenia: {{ $i->date_end }} r
+                                    </li>
+                                </ul>
+                                @if(1 == 2)
                                     <a href="{{ route('front.developro.completed.show', $i->slug) }}" class="custom-button z-2">Sprawdź</a>
-                                    @endif
-                                </div>
+                                @endif
                             </div>
                         </div>
-                    @endforeach
-                </div>
-                <!-- PAGINATION -->
-                <div class="row mt-40 mt-lg-80 mt-xl-125 d-none">
-                    <div class="col-12">
-                        <div class="pagination d-flex align-items-center justify-content-center gap-20 gap-md-50">
-                            <a href="#" class="pagination__btn">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="8.685" height="15.588" viewBox="0 0 8.685 15.588"><path id="arrow-left" d="M285.484-836.722l.891-.891-6.9-6.9,6.9-6.9-.891-.891-7.794,7.794Z" transform="translate(-277.69 852.31)" fill="#fff"></path></svg>
-                            </a>
-                            <span class="pagination__current">
-                                1
-                            </span>
-                            <a href="#" class="pagination__link">2</a>
-                            <a href="#" class="pagination__link">3</a>
-                            <a href="#" class="pagination__btn">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="8.685" height="15.588" viewBox="0 0 8.685 15.588"><path id="arrow-next" d="M278.581-836.722l-.891-.891,6.9-6.9-6.9-6.9.891-.891,7.794,7.794Z" transform="translate(-277.69 852.31)" fill="#fff"></path></svg>
-                            </a>
-                        </div>
                     </div>
-                </div>
-                <!-- END -> PAGINATION -->
+                @endforeach
             </div>
         </div>
-
-        <!-- FORM -->
-        @include('front.contact.offer-ask-form', ['pageTitle' => 'Inwestycje zrealizowane', 'back' => true])
-    </main>
+    </div>
     <!-- END -> MAIN SECTION -->
 @endsection
