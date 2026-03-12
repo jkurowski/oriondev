5ef5bc76b5f06646f6d9ee1831c80736b67c7ff5
Jacek Kurowski <jacek@4dl.pl>
2026-03-12 16:55:05 +0100

    [New][Jacek] RWD - Wykończenie mieszkań

--- resources/views/front/menupage/wykonczenie-mieszkan.blade.php
+++ resources/views/front/menupage/wykonczenie-mieszkan.blade.php
@@ -1,17 +1,25 @@
 @extends('layouts.page', ['body_class' => 'position-relative', 'body_id' => 'page-menupage'])
 
-@section('meta_title', $page->title ?? 'Relacje inwestorskie')
+@section('meta_title', $page->title ?? 'Wykończenie mieszkań')
 @section('seo_title',$page->meta_title ?? '')
 @section('seo_description', $page->meta_description ?? '')
 @section('seo_robots', $page->meta_robots ?? '')
 
 @section('content')
+    <div class="container-fluid mieszkania-submenu">
+        <div class="row">
+            <div class="col-12 text-center">
+                <h1>Wykończenie mieszkań</h1>
+            </div>
+        </div>
+    </div>
+
     <!-- MAIN SECTION -->
     <div id="page">
         <div class="container">
             <div class="row flex-row-reverse">
-                <div class="col-7 d-flex justify-content-end align-items-center">
-                    <div class="ps-5">
+                <div class="col-12 col-lg-7 d-flex justify-content-end align-items-center">
+                    <div class="ps-0 ps-lg-5">
                         <div class="section-header">
                             <span class="section-header-subtitle dm-sans-400">
                                 <i class="subtitle-left"><svg xmlns="http://www.w3.org/2000/svg" width="9" height="11" viewBox="0 0 9 11" fill="none"><path d="M0.527588 0V9.49634H8.96878" stroke="#FF9500" stroke-width="1"/></svg></i>
@@ -25,38 +33,38 @@
                         <p>Oferujemy możliwość współpracy z doświadczonymi specjalistami, którzy kompleksowo zajmą się zaprojektowaniem oraz wykończeniem mieszkania. Każdy projekt powstaje w oparciu o Państwa potrzeby, gust oraz styl życia, z jednoczesnym uwzględnieniem maksymalnej funkcjonalności i ergonomii przestrzeni.</p>
                     </div>
                 </div>
-                <div class="col-5 d-flex justify-content-start">
+                <div class="col-12 col-lg-5 d-flex justify-content-center justify-content-lg-start align-items-center mt-5 mt-lg-0">
                     <img src="{{ asset('images/kariera.webp') }}" alt="" class="img-rounded">
                 </div>
             </div>
         </div>
     </div>
 
-    <section class="pt-6 pb-6">
+    <section class="pt-5 pt-xxl-6 pb-5 pb-xxl-6 section-mobile">
         <div class="container">
             <div class="row mt-3">
-                <div class="col-3">
-                    <div class="iconbox pe-5">
+                <div class="col-12 col-sm-6 col-lg-3">
+                    <div class="iconbox pe-0 pe-sm-5">
                         <img src="images/icons/paint-roller.svg" alt="" width="76" height="76">
                         <h3>Odbiór<br> kluczy</h3>
                         <p>Początek drogi do stworzenia własnej, idealnie dopasowanej przestrzeni.</p>
                     </div>
                 </div>
-                <div class="col-3">
-                    <div class="iconbox pe-5">
+                <div class="col-12 col-sm-6 col-lg-3 mt-5 mt-sm-0">
+                    <div class="iconbox pe-0 pe-sm-5">
                         <img src="images/icons/paint-roller.svg" alt="" width="76" height="76">
                         <h3>Indywidualny<br> projekt</h3>
                         <p>Projekt wnętrza dopasowany do Państwa stylu życia i potrzeb.</p>
                     </div>
                 </div>
-                <div class="col-3">
-                    <div class="iconbox pe-5">
+                <div class="col-12 col-sm-6 col-lg-3 mt-5 mt-lg-0">
+                    <div class="iconbox pe-0 pe-sm-5">
                         <img src="images/icons/paint-roller.svg" alt="" width="76" height="76">
                         <h3>Kompleksowe<br> wykończenie</h3>
                         <p>Od projektu po realizację - wszystkim zajmuje się doświadczony zespół.</p>
                     </div>
                 </div>
-                <div class="col-3">
+                <div class="col-12 col-sm-6 col-lg-3 mt-5 mt-lg-0">
                     <div class="iconbox">
                         <img src="images/icons/paint-roller.svg" alt="" width="76" height="76">
                         <h3>Mieszkanie<br> pokazowe</h3>
@@ -70,8 +78,8 @@
     <div id="page">
         <div class="container">
             <div class="row">
-                <div class="col-7 d-flex align-items-center">
-                    <div class="pe-5">
+                <div class="col-12 col-lg-7 d-flex align-items-center">
+                    <div class="pe-0 pe-lg-5">
                         <div class="section-header">
                             <span class="section-header-subtitle dm-sans-400">
                                 <i class="subtitle-left"><svg xmlns="http://www.w3.org/2000/svg" width="9" height="11" viewBox="0 0 9 11" fill="none"><path d="M0.527588 0V9.49634H8.96878" stroke="#FF9500" stroke-width="1"/></svg></i>
@@ -85,12 +93,12 @@
                         <p>Aby mogli Państwo przekonać się o jakości pracy naszych specjalistów, z przyjemnością zaprezentujemy mieszkanie pokazowe. To doskonała okazja, by naocznie zapoznać się za standardem wykończenia, ocenić możliwości naszej ekipy oraz wyobrazić sobie efekt końcowy jeszcze przed rozpoczęciem realizacji.</p>
                     </div>
                 </div>
-                <div class="col-5 d-flex justify-content-end">
+                <div class="col-12 col-lg-5 d-flex justify-content-center justify-content-lg-end align-items-center mt-5 mt-lg-0">
                     <img src="{{ asset('images/kariera.webp') }}" alt="" class="img-rounded">
                 </div>
             </div>
 
-            <div class="row mt-6 pt-4">
+            <div class="row mt-4 mt-lg-6 pt-4">
                 <div class="col-12 text-center">
                     <h3>Poniżej prezentujemy kilka przykładowych<br> wizualizacji wykończenia mieszkania.</h3>
                 </div>
