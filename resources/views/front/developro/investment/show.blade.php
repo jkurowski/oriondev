0329ddceb565823dd94db0912e0fcbce27c933a9
Jacek Kurowski <jacek@4dl.pl>
2026-03-18 11:48:04 +0100

    [RWD][Jacek] O firmie

--- resources/views/front/developro/investment/show.blade.php
+++ resources/views/front/developro/investment/show.blade.php
@@ -19,15 +19,15 @@
         <div class="container">
             <div class="row">
                 @if($investment->status == 1)
-                <div class="col-2">
-                    <div class="sidemenu">
+                <div class="col-12 col-lg-3 col-xxl-2">
+                    <div class="sidemenu d-none d-lg-block">
                         @include('front.developro.investment_shared.menu')
                     </div>
                 </div>
                 @endif
-                <div class="@if($investment->status == 1) col-10 @else col-12 @endif">
-                    <div class="row ps-5">
-                        <div class="col-6 pe-5 d-flex align-items-center justify-content-center">
+                <div class="@if($investment->status == 1) col-12 col-lg-9 col-xxl-10 @else col-12 @endif">
+                    <div class="row ps-0 ps-lg-3 ps-xl-5">
+                        <div class="col-12 col-xxl-6 pe-0 pe-xxl-5 mb-5 mb-xxl-0 d-flex align-items-center justify-content-center">
                             <div>
                                 {!! $investment->content !!}
                                 @if($investment->plan)
@@ -35,7 +35,7 @@
                                 @endif
                             </div>
                         </div>
-                        <div class="col-6">
+                        <div class="col-12 col-xxl-6">
                             <div class="apartament-slider__slider-for mb-3">
                                 @if($investment->images->count() < 5)
                                     @foreach($investment->images as $image)
@@ -96,13 +96,13 @@
     </div>
 
     @if($investment->advantage_content)
-        <div class="container-fluid apartament-text position-relative pt-6 pb-6">
+        <div class="container-fluid apartament-text position-relative pt-6 pb-6 section-mobile">
             <div class="container">
                 <div class="row">
                     <div class="col-12 col-xl-6 col-xxl-5 d-flex flex-column align-items-start justify-content-center order-2 order-xl-1 mt-30 mt-xl-0 scroll-anim-top">
                         {!! $investment->advantage_content !!}
                     </div>
-                    <div class="col-12 col-lg-12 col-xl-6 offset-0 offset-xl-0 offset-xxl-1 order-1 order-xl-2 scroll-anim-bottom">
+                    <div class="col-12 col-lg-12 col-xl-6 offset-0 offset-xl-0 offset-xxl-1 order-1 order-xl-2 mb-5 mb-xl-0">
                         <img src="{{ asset('/investment/advantage/'.$investment->file_advantage) }}" alt="Wizualizacja inwestycji {{ $investment->name }}" class="border-blue">
                     </div>
                 </div>
@@ -112,24 +112,22 @@
 
     <div id="page">
         @if($investment->location_content)
-            <div class="container lokalizacja"  data-map="true">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-12 col-lg-6 ">
-                            <div class="map-container">
-                                <div id="locationmap" class="w-100 h-100"></div>
-                            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 col-lg-6 ">
+                        <div class="map-container">
+                            <div id="locationmap" class="w-100 h-100"></div>
                         </div>
-                        <div class="col-12 col-lg-6 d-flex align-items-center justify-content-center">
-                            <div class="ps-5">
-                                <div class="section-header">
-                                    <h2 class="big">Lokalizacja</h2>
-                                </div>
-                                {!! $investment->location_content !!}
-                                @if($investment->status == 1)
-                                <a href="{{ route('front.developro.page', [$investment->slug, 'lokalizacja']) }}" class="bttn bttn-white bttn-icon mt-5">Zobacz więcej <span><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18" fill="none"><g clip-path="url(#sendIcon)"><path d="M4.9776 4.25018L4.97086 6.26437L9.35486 6.26437L3.55046 12.0688L4.96731 13.4856L10.7717 7.68122L10.7717 12.0652L12.7859 12.0585L12.777 4.25905L4.9776 4.25018Z"></path></g><defs><clipPath id="sendIcon"><rect width="12.0465" height="12.0465" transform="translate(0 8.51855) rotate(-45)"></rect></clipPath></defs></svg></span></a>
-                                @endif
+                    </div>
+                    <div class="col-12 col-lg-6 d-flex align-items-center justify-content-center mt-3 mt-lg-0">
+                        <div class="ps-0 ps-lg-5">
+                            <div class="section-header">
+                                <h2 class="big">Lokalizacja</h2>
                             </div>
+                            {!! $investment->location_content !!}
+                            @if($investment->status == 1)
+                                <a href="{{ route('front.developro.page', [$investment->slug, 'lokalizacja']) }}" class="bttn bttn-white bttn-icon mt-3 mt-md-5">Zobacz więcej <span><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18" fill="none"><g clip-path="url(#sendIcon)"><path d="M4.9776 4.25018L4.97086 6.26437L9.35486 6.26437L3.55046 12.0688L4.96731 13.4856L10.7717 7.68122L10.7717 12.0652L12.7859 12.0585L12.777 4.25905L4.9776 4.25018Z"></path></g><defs><clipPath id="sendIcon"><rect width="12.0465" height="12.0465" transform="translate(0 8.51855) rotate(-45)"></rect></clipPath></defs></svg></span></a>
+                            @endif
                         </div>
                     </div>
                 </div>
--- resources/views/front/developro/investment_page/index.blade.php
+++ resources/views/front/developro/investment_page/index.blade.php
@@ -18,13 +18,13 @@
     <div id="page">
         <div class="container">
             <div class="row">
-                <div class="col-2">
-                    <div class="sidemenu sticky-top">
+                <div class="col-12 col-lg-3 col-xxl-2">
+                    <div class="sidemenu sticky-top d-none d-lg-block">
                         @include('front.developro.investment_shared.menu')
                     </div>
                 </div>
-                <div class="col-10">
-                    <div class="ps-5 mieszkania-page">
+                <div class="col-12 col-lg-9 col-xxl-10">
+                    <div class="ps-0 ps-lg-3 ps-xl-5 mieszkania-page">
                         <h2 class="mieszkania-page-title">{{ $investment_page->title }}</h2>
                         {!! parse_text($investment_page->content) !!}
                     </div>
